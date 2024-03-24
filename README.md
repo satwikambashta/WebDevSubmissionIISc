0ef9d793ef832539e79d34bcc1d0e9c5cdb87283
satwikambashta <simmysatwik03@gmail.com>
2024-03-24 19:01:55 +0530

    styling completed

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
+# Face Detection App
+
+This application demonstrates face detection using OpenCV in a Flask backend and a Vite React frontend.
+
+## Backend Setup
+
+Navigate to the `backend` folder and install the required dependencies:
+in a virtual environment or install globally using:
+`pip install -r requirements.txt`
+
+or
+
+``` 
+pip install Flask-CORS
+pip install flask   
+pip install opencv-python
+```
+
+## Frontend Setup
+
+Navigate to the `frontend` folder and install the required dependencies:
+
+`npm i`
+
+## Backend start server:
+nagivate to the backend folder by `cd server`
+`python main.py`
+
+## Frontend development server:
+`npm run dev`
+
+## Navigate to localhost:5173 to access the website
+
+<!-- in the backend folder(server):
 pip install Flask-CORS
 pip install flask   
 pip install opencv-python
 
-npm install axios   
+in the frontend folder(client):
+npm i 
 
 
 To run: 
@@ -11,4 +46,4 @@ in the backend folder(server):
 python main.py 
 
 in the frontend folder(client):
-npm run dev
+npm run dev -->
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react';
+import React, { useState, useRef } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [imageSrc, setImageSrc] = useState('');
+  const [imageSrc, setImageSrc] = useState("");
+  const [originalImageSrc, setOriginalImageSrc] = useState("");
   const [uploadProgress, setUploadProgress] = useState(0);
+  const [imageUploaded, setImageUploaded] = useState(false);
+
+  const fileInputRef = useRef(null);
+
+  const triggerFileInput = () => {
+    fileInputRef.current.click();
+  };
 
   const resizeImage = (file, maxWidth, maxHeight) => {
     return new Promise((resolve, reject) => {
       const image = new Image();
       image.src = URL.createObjectURL(file);
       image.onload = () => {
-        const canvas = document.createElement('canvas');
+        const canvas = document.createElement("canvas");
         let width = image.width;
         let height = image.height;
 
@@ -29,7 +37,7 @@ function App() {
 
         canvas.width = width;
         canvas.height = height;
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext("2d");
         ctx.drawImage(image, 0, 0, width, height);
 
         canvas.toBlob((blob) => {
@@ -43,42 +51,101 @@ function App() {
     const file = event.target.files[0];
     if (!file) return;
 
+    setOriginalImageSrc(URL.createObjectURL(file));
+
     const resizedImage = await resizeImage(file, 800, 800);
 
     const formData = new FormData();
-    formData.append('file', resizedImage, file.name);
+    formData.append("file", resizedImage, file.name);
 
-    const response = await axios.post("http://127.0.0.1:8080/api/upload", formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-      onUploadProgress: (progressEvent) => {
-        const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-        setUploadProgress(percentCompleted);
+    const response = await axios.post(
+      "http://127.0.0.1:8080/api/upload",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setUploadProgress(percentCompleted);
+        },
       }
-    });
+    );
 
     if (response.data.filename) {
       setImageSrc(`http://127.0.0.1:8080/images/${response.data.filename}`);
+      setImageUploaded(true);
     }
   };
 
   return (
     <div className="app-container">
-      <div>
-        <div className="progress-bar">
-          <div className="progress" style={{ width: `${uploadProgress}%` }}></div>
+      <div className="title">
+        <h1>Face Detection with OpenCV</h1>
+        <div className="upload-container">
+          <input
+            type="file"
+            className="custom-file-input"
+            onChange={uploadImage}
+            ref={fileInputRef}
+          />
+          {!originalImageSrc && (
+            <div className="uploadimgfirst">
+              <h3>Upload an image </h3>
+              <button
+                type="button"
+                className="custom-file-button"
+                onClick={triggerFileInput}
+              >
+                Upload Image
+              </button>
+            </div>
+          )}
         </div>
       </div>
-      <h1>Upload and Display Image</h1>
-      <div className="upload-container">
-        <input type="file" onChange={uploadImage} />
-        {imageSrc && <img src={imageSrc} alt="Uploaded" />}
-        {imageSrc && <a href={imageSrc} download="downloaded-image.jpg"><button>Download Image</button></a>}
+
+      <div className="images-container">
+        <div className="uploaded-image">
+          {imageSrc && <h3>Original Image</h3>}
+          {originalImageSrc && <img src={originalImageSrc} alt="Original" />}
+          <div className="upload-container">
+            <input
+              type="file"
+              className="custom-file-input"
+              onChange={uploadImage}
+              ref={fileInputRef}
+            />
+            {originalImageSrc && (
+              <button
+                type="button"
+                className="custom-file-button"
+                onClick={triggerFileInput}
+              >
+                Upload Image
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="processed-image">
+          {imageSrc && <h3>Processed Image</h3>}
+          {imageSrc && <img src={imageSrc} alt="Processed" />}
+          {imageSrc && (
+            <a href={imageSrc} download="downloaded-image.jpg">
+              <button className="custom-file-button">Download Image</button>
+            </a>
+          )}
+        </div>
+      </div>
+
+      <div className="footer">
+        <p>
+          &copy; <a href="https://github.com/satwikambashta">Satwik Saurav</a>{" "}
+        </p>
       </div>
     </div>
   );
-
 }
 
 export default App;
